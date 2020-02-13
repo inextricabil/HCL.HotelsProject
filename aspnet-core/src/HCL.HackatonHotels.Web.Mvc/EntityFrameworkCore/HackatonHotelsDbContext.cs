7977b2e1e4242bc331242f5757e42b181d3e13f2
Serban Boghiu <pincellodev14@outlook.com>
2020-02-13 12:17:51 +0200

    WIP: database working

---
+++ aspnet-core/src/HCL.HackatonHotels.Web.Mvc/EntityFrameworkCore/HackatonHotelsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using HCL.HackatonHotels.Authorization.Roles;
+using HCL.HackatonHotels.Authorization.Users;
+using HCL.HackatonHotels.MultiTenancy;
+
+namespace HCL.HackatonHotels.EntityFrameworkCore
+{
+    public class HackatonHotelsDbContext : AbpZeroDbContext<Tenant, Role, User, HackatonHotelsDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public HackatonHotelsDbContext(DbContextOptions<HackatonHotelsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
